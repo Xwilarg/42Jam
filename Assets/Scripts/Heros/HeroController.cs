214df7ff91d1a6886acb9d12a9877968540a8267
Xwilarg <xwilarg@yahoo.fr>
2019-11-30 17:50:49 +0900

    Add hero status

--- Assets/Scripts/Heros/HeroController.cs
+++ Assets/Scripts/Heros/HeroController.cs
@@ -2,22 +2,24 @@
 
 public class HeroController : MonoBehaviour
 {
+    [SerializeField]
+    private TextMesh infos;
+    private SpriteRenderer sr; // Move sr.transform instead of transform
     private Transform player;
-
     private HeroClass heroClass;
-
     private string heroName;
     private const int avoidPlayerLayer = ~(1 << 8 | 1 << 10);
 
     public void Init(string nameValue, HeroClass heroValue)
     {
         heroName = nameValue;
-        heroClass = heroValue;
+        heroClass = heroValue;        infos.text = "Name: " + heroName + "\nClass: " + heroClass;
     }
 
     private void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        sr = GetComponentInChildren<SpriteRenderer>();
     }
 
     private void Update()
@@ -28,12 +30,22 @@ public class HeroController : MonoBehaviour
         }
     }
 
+    private void OnMouseEnter()
+    {
+        infos.gameObject.SetActive(true);
+    }
+
+    private void OnMouseExit()
+    {
+        infos.gameObject.SetActive(false);
+    }
+
     public enum HeroClass
     {
         Warrior,
         Healer,
         Rogue,
-        Bowman,
+        Archer,
         Mage
     }
 }
