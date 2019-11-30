03048584af236666c772e9a73eba7204df56ae78
SkyCobra <44617427+SkyCobra@users.noreply.github.com>
2019-11-30 13:41:55 +0900

    [ADD] add Spawner

---
+++ Assets/Scripts/Trap/Spawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject gobelin;
+    public float rate;
+    public int maxLevel;
+    public int Level;
+    public List<Sprite> upgradeSprite;
+    public int Cost;
+    private Character player;
+    public GameObject panel;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+        //StartCoroutine(cooldown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+            Upgrade();
+        
+    }
+
+    IEnumerator cooldown()
+    {
+        yield return new WaitForSeconds(rate);
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        Instantiate(gobelin, transform);
+        StartCoroutine(cooldown());
+    }
+
+    public void Upgrade()
+    {
+        if (player.GetOr() >= Cost)
+        {
+            GetComponent<SpriteRenderer>().sprite = upgradeSprite[Level];
+        }
+        else
+            Debug.Log("Not enough Gold");
+    }
+
+    public void Delete()
+    {
+        player.GainOr(Cost * Level);
+        Destroy(this.gameObject);
+    }
+
+    private void OnMouseDown()
+    {
+        panel.SetActive(true);
+    }
+}
