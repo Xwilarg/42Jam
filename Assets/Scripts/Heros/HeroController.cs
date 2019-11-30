c4ac38e3c15f63ea0e205e81409a131f13edbf4b
Xwilarg <xwilarg@yahoo.fr>
2019-11-30 14:16:33 +0900

    Add hero prefab

---
+++ Assets/Scripts/Heros/HeroController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class HeroController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private HeroClass heroClass;
+
+    private const int avoidPlayerLayer = ~(1 << 8 | 1 << 10);
+
+    private void Update()
+    {
+        if (!Physics2D.Linecast(transform.position, player.position, avoidPlayerLayer)) // Can see player, battle mode
+        {
+
+        }
+    }
+
+    private enum HeroClass
+    {
+        Warrior,
+        Healer,
+        Rogue,
+        Bowman,
+        Mage
+    }
+}
