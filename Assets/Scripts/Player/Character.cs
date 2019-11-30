5b2d47ed2aa53c1c966d0a3a265d07b36a179c04
Xwilarg <xwilarg@yahoo.fr>
2019-11-30 12:49:42 +0900

    Add sword attack

--- Assets/Scripts/Player/Character.cs
+++ Assets/Scripts/Player/Character.cs
@@ -5,6 +5,23 @@ public class Character : MonoBehaviour
     [SerializeField]
     private int hp;
 
+    private int or;
+
+    private void Start()
+    {
+        or = 0;
+    }
+
     public void LooseHp(int value)
-        => hp -= value;
+    {
+        hp -= value;
+        if (hp <= 0)
+            Destroy(gameObject);
+    }
+
+    public int GetOr()
+        => or;
+
+    public int GainOr(int value)
+        => or += value;
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,10 +4,18 @@
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    [Tooltip("Player speed")]
+    [Tooltip("Player Speed")]
     private float speed;
 
+    [SerializeField]
+    [Tooltip("Damage Display Prefab")]
+    private GameObject damagePrefab;
+
     private Rigidbody2D rb;
+
+    private const float swordRange = 1f;
+    private const int avoidPlayerLayer = ~(1 << 8);
+    private const int swordDamage = 10;
 
     private void Start()
     {
@@ -17,6 +25,8 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+
+        // Set player orientation
         if (Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y))
         {
             if (rb.velocity.x > 0f)
@@ -30,6 +40,20 @@ public class PlayerController : MonoBehaviour
                 transform.rotation = Quaternion.Euler(0f, 0f, -90f);
             else if (rb.velocity.y < 0f)
                 transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+
+        // Attacks
+        if (Input.GetKeyDown(KeyCode.Z)) // Sword attack
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.position - transform.right, swordRange, avoidPlayerLayer);
+            if (hit.collider != null)
+            {
+                Character charac = hit.collider.GetComponent<Character>();
+                charac.LooseHp(swordDamage);
+                GameObject go = Instantiate(damagePrefab, hit.point, Quaternion.identity);
+                go.GetComponent<TextMesh>().text = "-" + swordDamage;
+                go.transform.position = new Vector3(transform.position.x, transform.position.y, -1f);
+            }
         }
     }
 }
