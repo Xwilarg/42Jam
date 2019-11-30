5b51da6702cd2b66564b2caceb09f27036382bca
Xwilarg <xwilarg@yahoo.fr>
2019-11-30 12:34:22 +0900

    Add damage display

---
+++ Assets/Scripts/Effects/FadeTextMesh.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class FadeTextMesh : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private TextMesh text;
+
+    private void Start()
+    {
+        text = GetComponent<TextMesh>();
+    }
+
+    private void FixedUpdate()
+    {
+        float newAlpha = text.color.a - speed;
+        if (newAlpha < 0f)
+            Destroy(gameObject);
+        else
+            text.color = new Color(text.color.r, text.color.g, text.color.b, newAlpha);
+    }
+}
---
+++ Assets/Scripts/Effects/GoUp.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class GoUp : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private void FixedUpdate()
+    {
+        transform.Translate(0f, speed, 0f);
+    }
+}
