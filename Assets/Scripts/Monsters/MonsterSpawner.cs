064ca34c958bf17ac6835ce42c96b7a5af6e41aa
YohanJabin <yohan.jabin@epitech.eu>
2019-11-30 23:56:27 +0900

    Add basic monster scripts and prefabs

---
+++ Assets/Scripts/Monsters/MonsterSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    public GameObject monsterToSpawn;
+    public uint numberToSpawn;
+    
+    void Start()
+    {
+        for (int i = 0; i < numberToSpawn; i++) {
+            Instantiate(monsterToSpawn, transform.position, Quaternion.identity, transform);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
