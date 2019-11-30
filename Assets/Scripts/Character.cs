ec287d3508da08a9206e5a555a3ba8bd135a8e6c
Xwilarg <xwilarg@yahoo.fr>
2019-11-30 12:26:59 +0900

    Add player controller

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private int hp;
+
+    public void LooseHp(int value)
+        => hp -= value;
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,4 +3,33 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("Player speed")]
+    private float speed;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * speed;
+        if (Mathf.Abs(rb.velocity.x) > Mathf.Abs(rb.velocity.y))
+        {
+            if (rb.velocity.x > 0f)
+                transform.rotation = Quaternion.Euler(0f, 0f, 180f);
+            else if (rb.velocity.x < 0f)
+                transform.rotation = Quaternion.identity;
+        }
+        else
+        {
+            if (rb.velocity.y > 0f)
+                transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+            else if (rb.velocity.y < 0f)
+                transform.rotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+    }
 }
