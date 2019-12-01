f9ecc2ffecdb0445136b4f85b13bbd7ff85122a8
Xwilarg <xwilarg@yahoo.fr>
2019-12-02 04:16:33 +0900

    Reduce fireball range

--- Assets/Scripts/Player/Fireball.cs
+++ Assets/Scripts/Player/Fireball.cs
@@ -4,7 +4,7 @@ public class Fireball : Bullet
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        foreach (Collider2D collider in Physics2D.OverlapCircleAll(transform.position, 3f))
+        foreach (Collider2D collider in Physics2D.OverlapCircleAll(transform.position, 2f))
         {
             collider.GetComponent<Character>()?.LooseHp(damage);
         }
