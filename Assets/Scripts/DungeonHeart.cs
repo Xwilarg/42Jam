284b0be4eac7c89b0518999e6d8c407ef0b140a6
Xwilarg <xwilarg@yahoo.fr>
2019-12-02 01:05:51 +0900

    Add defeat when enemy go on heart

---
+++ Assets/Scripts/DungeonHeart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DungeonHeart : MonoBehaviour
+{
+    private Character player;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Character>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Hero"))
+            player.LooseHp(9999);
+    }
+}
